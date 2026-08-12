f5a0347d25fa27aade5474972ef40aa0457fa92d
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-08-12 12:24:53 +0200

    feat: validate other tumor staging classification values (#11)

--- dnpmvalidation/src/validation/content/mod.rs
+++ dnpmvalidation/src/validation/content/mod.rs
@@ -1,3 +1,5 @@
+mod diagnosis;
+
 use crate::validation::{map_query_ref, map_to_validation_error};
 use crate::{ValidationError, ValidationType};
 use jsonpath_rust::JsonPath;
@@ -24,12 +26,7 @@ pub fn validate(
     )?);
 
     if validation_type == ValidationType::Mtb {
-        errors.append(&mut validate_contains_oneof(
-            json,
-            "tumor staging classification",
-            &["tnmClassification", "otherClassifications"],
-            "$.diagnoses[*].staging.history[*]",
-        )?);
+        errors.append(&mut diagnosis::validate(json, validation_type)?);
     }
 
     Ok(errors)
@@ -87,12 +84,58 @@ fn validate_contains_oneof(
             let Some(obj) = item.val.as_object() else {
                 return true;
             };
-            !keys.iter().any(|&subpath| { obj.contains_key(subpath) })
+            !keys.iter().any(|&subpath| obj.contains_key(subpath))
         })
         .map(map_query_ref)
         .map(|(err_path, _)| {
             map_to_validation_error(
-                (err_path, format!("Missing {name}: should contain one of {}", keys.join(", "))),
+                (
+                    err_path,
+                    format!("Missing {name}: should contain one of {}", keys.join(", ")),
+                ),
+                json,
+                &mut parser,
+            )
+        })
+        .collect::<Vec<_>>();
+
+    errors.append(&mut value);
+
+    Ok(errors)
+}
+
+fn validate_contains_valueof(
+    json: &str,
+    name: &str,
+    values: &[&str],
+    path: &str,
+) -> Result<Vec<ValidationError>, Box<dyn std::error::Error>> {
+    let value = serde_json::from_str::<Value>(json)?;
+
+    let mut errors = Vec::new();
+
+    let mut parser = Parser::new();
+    parser.set_language(&tree_sitter_json::LANGUAGE.into())?;
+
+    let mut value = value
+        .query_with_path(path)?
+        .iter()
+        .filter(|item| {
+            let Some(value) = item.val.as_str() else {
+                return true;
+            };
+            !values.iter().any(|&expected_value| expected_value == value)
+        })
+        .map(map_query_ref)
+        .map(|(err_path, value)| {
+            map_to_validation_error(
+                (
+                    err_path,
+                    format!(
+                        "Invalid {name} '{value}': should be one of {}",
+                        values.join(", ")
+                    ),
+                ),
                 json,
                 &mut parser,
             )
@@ -210,104 +253,5 @@ mod tests {
         assert_eq!(actual[0].message, "Invalid ATC code 'Testonimib A'");
         assert_eq!(actual[1].message, "Invalid ATC code 'Testonimib B'");
         assert_eq!(actual[2].message, "Invalid ATC code 'Testonimib C'");
-    }
-
-    #[test]
-    fn test_should_find_missing_tumor_staging_classification_errors() {
-        let json = json!({
-            "diagnoses": [{
-                "staging": {
-                    "history": [
-                      // Valid: TNM and Other
-                      {
-                        "date": "2022-10-01",
-                        "method": {
-                          "code": "clinical",
-                          "display": "Klinisch",
-                          "system": "dnpm-dip/mtb/tumor-staging/method"
-                        },
-                        "tnmClassification": {
-                          "tumor": {
-                            "code": "T2",
-                            "system": "UICC"
-                          },
-                          "nodes": {
-                            "code": "N0",
-                            "system": "UICC"
-                          },
-                          "metastasis": {
-                            "code": "MX",
-                            "system": "UICC"
-                          }
-                        },
-                        "otherClassifications": [
-                          {
-                            "code": "metastasized",
-                            "display": "Metastasiert",
-                            "system": "dnpm-dip/mtb/diagnosis/kds-tumor-spread"
-                          }
-                        ]
-                      },
-                      // Valid: Other only
-                      {
-                        "date": "2022-11-01",
-                        "method": {
-                          "code": "clinical",
-                          "display": "Klinisch",
-                          "system": "dnpm-dip/mtb/tumor-staging/method"
-                        },
-                        "otherClassifications": [
-                          {
-                            "code": "metastasized",
-                            "display": "Metastasiert",
-                            "system": "dnpm-dip/mtb/diagnosis/kds-tumor-spread"
-                          }
-                        ]
-                      },
-                      // Valid: TNM only
-                      {
-                        "date": "2022-12-01",
-                        "method": {
-                          "code": "clinical",
-                          "display": "Klinisch",
-                          "system": "dnpm-dip/mtb/tumor-staging/method"
-                        },
-                        "tnmClassification": {
-                          "tumor": {
-                            "code": "T2",
-                            "system": "UICC"
-                          },
-                          "nodes": {
-                            "code": "N0",
-                            "system": "UICC"
-                          },
-                          "metastasis": {
-                            "code": "MX",
-                            "system": "UICC"
-                          }
-                        }
-                      },
-                      // INVALID!
-                      {
-                        "date": "2023-11-19",
-                        "method": {
-                          "code": "clinical",
-                          "display": "Klinisch",
-                          "system": "dnpm-dip/mtb/tumor-staging/method"
-                        }
-                      }
-                    ]
-                  }
-            }]
-        })
-            .to_string();
-
-        let actual = validate(&json, ValidationType::Mtb);
-
-        assert!(actual.is_ok());
-
-        let actual = actual.expect("available validation results");
-        assert_eq!(actual.len(), 1);
-        assert_eq!(actual[0].message, "Missing tumor staging classification: should contain one of tnmClassification, otherClassifications");
     }
 }
