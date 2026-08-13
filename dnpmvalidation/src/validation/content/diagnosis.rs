e6a4681ad375357b84aae1ac8a3c24e7331fde9c
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-08-13 10:50:33 +0200

    feat: validation for tumor grading (#13)

--- dnpmvalidation/src/validation/content/diagnosis.rs
+++ dnpmvalidation/src/validation/content/diagnosis.rs
@@ -13,6 +13,38 @@ pub fn validate(
     }
 
     if validation_type == ValidationType::Mtb {
+        errors.append(&mut validate_contains_valueof(
+            json,
+            "tumor grading code",
+            &["0", "1", "2", "3", "4", "5", "X", "L", "M", "H", "B", "U", "T"],
+            "$.diagnoses[*].grading.history[*].codes[?(@.system == 'https://www.basisdatensatz.de/feld/161/grading')].code",
+            &Severity::Error,
+        )?);
+
+        errors.append(&mut validate_contains_valueof(
+            json,
+            "tumor grading code",
+            &["I", "II", "III", "IV"],
+            "$.diagnoses[*].grading.history[*].codes[?(@.system == 'dnpm-dip/mtb/who-grading-cns-tumors' && @.version == '2016')].code",
+            &Severity::Error,
+        )?);
+
+        errors.append(&mut validate_contains_valueof(
+            json,
+            "tumor grading code",
+            &["1", "2", "3", "4"],
+            "$.diagnoses[*].grading.history[*].codes[?(@.system == 'dnpm-dip/mtb/who-grading-cns-tumors' && @.version == '2021')].code",
+            &Severity::Error,
+        )?);
+
+        errors.append(&mut validate_contains_valueof(
+            json,
+            "tumor grading version",
+            &["2016", "2021"],
+            "$.diagnoses[*].grading.history[*].codes[*].version",
+            &Severity::Error,
+        )?);
+
         errors.append(&mut validate_contains_oneof(
             json,
             "tumor staging classification",
@@ -218,6 +250,141 @@ mod tests {
         assert_eq!(
             actual[0].message,
             "Invalid tumor staging classification value 'wrong!': must be one of tumor-free, local, metastasized"
+        );
+    }
+
+    #[test]
+    fn test_should_find_invalid_tumor_grading_cns_codes() {
+        let json = json!({
+            "diagnoses": [{
+                "grading": {
+                    "history": [
+                      {
+                        "date": "2022-11-19",
+                        "codes": [
+                          {
+                            "code": "B",
+                            "display": "B = Borderline",
+                            "system": "https://www.basisdatensatz.de/feld/161/grading"
+                          },
+                          {
+                            "code": "3",
+                            "display": "Anaplastic astrocytoma",
+                            "system": "dnpm-dip/mtb/who-grading-cns-tumors",
+                            "version": "2021"
+                          }
+                        ]
+                      },
+                      // Invalid code for version 2016
+                      {
+                        "date": "2022-11-19",
+                        "codes": [
+                          {
+                            "code": "B",
+                            "display": "B = Borderline",
+                            "system": "https://www.basisdatensatz.de/feld/161/grading"
+                          },
+                          {
+                            "code": "3",
+                            "display": "Anaplastic astrocytoma",
+                            "system": "dnpm-dip/mtb/who-grading-cns-tumors",
+                            "version": "2016"
+                          }
+                        ]
+                      },
+                      // Invalid code for version 2021
+                      {
+                        "date": "2022-11-19",
+                        "codes": [
+                          {
+                            "code": "B",
+                            "display": "B = Borderline",
+                            "system": "https://www.basisdatensatz.de/feld/161/grading"
+                          },
+                          {
+                            "code": "IV",
+                            "display": "Anaplastic astrocytoma",
+                            "system": "dnpm-dip/mtb/who-grading-cns-tumors",
+                            "version": "2021"
+                          }
+                        ]
+                      }
+                    ]
+                  }
+            }]
+        })
+        .to_string();
+
+        let actual = validate(&json, ValidationType::Mtb);
+
+        assert!(actual.is_ok());
+
+        let actual = actual.expect("available validation results");
+        assert_eq!(actual.len(), 2);
+        assert_eq!(
+            actual[0].message,
+            "Invalid tumor grading code '3': must be one of I, II, III, IV"
+        );
+        assert_eq!(
+            actual[1].message,
+            "Invalid tumor grading code 'IV': must be one of 1, 2, 3, 4"
+        );
+    }
+
+    #[test]
+    fn test_should_find_invalid_tumor_grading_cns_versions() {
+        let json = json!({
+            "diagnoses": [{
+                "grading": {
+                    "history": [
+                      {
+                        "date": "2022-11-19",
+                        "codes": [
+                          {
+                            "code": "B",
+                            "display": "B = Borderline",
+                            "system": "https://www.basisdatensatz.de/feld/161/grading"
+                          },
+                          {
+                            "code": "3",
+                            "display": "Anaplastic astrocytoma",
+                            "system": "dnpm-dip/mtb/who-grading-cns-tumors",
+                            "version": "2021"
+                          }
+                        ]
+                      },
+                      // Invalid version '2026'
+                      {
+                        "date": "2022-11-19",
+                        "codes": [
+                          {
+                            "code": "B",
+                            "display": "B = Borderline",
+                            "system": "https://www.basisdatensatz.de/feld/161/grading"
+                          },
+                          {
+                            "code": "3",
+                            "display": "Anaplastic astrocytoma",
+                            "system": "dnpm-dip/mtb/who-grading-cns-tumors",
+                            "version": "2026"
+                          }
+                        ]
+                      }
+                    ]
+                  }
+            }]
+        })
+        .to_string();
+
+        let actual = validate(&json, ValidationType::Mtb);
+
+        assert!(actual.is_ok());
+
+        let actual = actual.expect("available validation results");
+        assert_eq!(actual.len(), 1);
+        assert_eq!(
+            actual[0].message,
+            "Invalid tumor grading version '2026': must be one of 2016, 2021"
         );
     }
 }
